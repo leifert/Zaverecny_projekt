fd3218c5229d8c69b9160a2846cb9c181a39fb29
leifert <32055635+leifert@users.noreply.github.com>
2018-11-24 23:17:02 +0100

    dht+mq135

--- meric-kvality-ovzdusi/src/main.cpp
+++ meric-kvality-ovzdusi/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Homie.h>
 #include "DHT.h"
+#include <MQ135.h>
 
+#define MQ135PIN A0
 #define DHTPIN 5
 #define DHTTYP DHT11
 #define INTERVAL 5 //sekundy
@@ -8,29 +10,31 @@
 unsigned long lastSent = 0;
 
 DHT dht(DHTPIN, DHTTYP);
+MQ135 plynovySensor = MQ135(MQ135PIN);
 
 HomieNode teplotaNode("teplota", "teplota");
 HomieNode vlhkostNode("vlhkost", "vlhkost");
+HomieNode co2Node("CO2", "CO2");
+HomieNode rzeroNode("rzero", "rzero");
 
 void setupHandler() {
   teplotaNode.setProperty("unit").send("C");
   vlhkostNode.setProperty("unit").send("%");
-
+  co2Node.setProperty("unit").send("PPM");
+  rzeroNode.setProperty("unit").send("rzero");
 }
 
 void loopHandler() {
-   if (millis() - lastSent >= INTERVAL * 1000UL || lastSent == 0) {
-     float teplota = dht.readTemperature();
-     float vlhkost = dht.readHumidity();
-     if (isnan(teplota) || isnan(vlhkost)) {
-      Homie.getLogger() << F("Chyba čtení ze snímače DHT!");
-    } else {
-      Homie.getLogger() << F("Teplota: ") << teplota << " °C" << endl;
-      Homie.getLogger() << F("Vlhkost: ") << vlhkost << " %" << endl;
+    if (millis() - lastSent >= INTERVAL * 1000UL || lastSent == 0) {
+      float teplota = dht.readTemperature();
+      float vlhkost = dht.readHumidity();
+      float rzero = plynovySensor.getRZero();
+      float ppm = plynovySensor.getPPM();
       teplotaNode.setProperty("stupne").send(String(teplota));
       vlhkostNode.setProperty("procenta").send(String(vlhkost));
-    }
-     lastSent = millis();
+      rzeroNode.setProperty("rzero").send(String(rzero));
+      co2Node.setProperty("PPM").send(String(ppm));
+        lastSent = millis();
   }
 }
 
