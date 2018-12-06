b4398610c476431a814518854db862a5e531306e
leifert <32055635+leifert@users.noreply.github.com>
2018-12-06 17:48:00 +0100

    mq135+homie

--- meric-kvality-ovzdusi/src/main.cpp
+++ meric-kvality-ovzdusi/src/main.cpp
@@ -1,39 +1,44 @@
 #include <Homie.h>
-#include "DHT.h"
-#include <MQ135.h>
+#include <DHT.h>
+#include <TroykaMQ.h>
+
 
 #define MQ135PIN A0
 #define DHTPIN 5
 #define DHTTYP DHT11
-#define INTERVAL 5 //sekundy
+#define INTERVAL 2 //sekundy
 
 unsigned long lastSent = 0;
 
 DHT dht(DHTPIN, DHTTYP);
-MQ135 plynovySensor = MQ135(MQ135PIN);
+MQ135 mq135(MQ135PIN);
+
 
 HomieNode teplotaNode("teplota", "teplota");
 HomieNode vlhkostNode("vlhkost", "vlhkost");
 HomieNode co2Node("CO2", "CO2");
-HomieNode rzeroNode("rzero", "rzero");
+HomieNode ratioNode("ratio", "ratio");
 
 void setupHandler() {
   teplotaNode.setProperty("unit").send("C");
   vlhkostNode.setProperty("unit").send("%");
   co2Node.setProperty("unit").send("PPM");
-  rzeroNode.setProperty("unit").send("rzero");
+  ratioNode.setProperty("unit").send("ratio");
+  mq135.calibrate();
+  mq135.getRo();
 }
 
 void loopHandler() {
     if (millis() - lastSent >= INTERVAL * 1000UL || lastSent == 0) {
       float teplota = dht.readTemperature();
       float vlhkost = dht.readHumidity();
-      float rzero = plynovySensor.getRZero();
-      float ppm = plynovySensor.getPPM();
-      teplotaNode.setProperty("stupne").send(String(teplota));
-      vlhkostNode.setProperty("procenta").send(String(vlhkost));
-      rzeroNode.setProperty("rzero").send(String(rzero));
-      co2Node.setProperty("PPM").send(String(ppm));
+
+      float ratio = mq135.readRatio();
+      float co2 = mq135.readCO2();
+      teplotaNode.setProperty("celsia").send(String(teplota));
+      vlhkostNode.setProperty("procent").send(String(vlhkost));
+      ratioNode.setProperty("ratio").send(String(ratio));
+      co2Node.setProperty("PPM").send(String(co2));
         lastSent = millis();
   }
 }
@@ -46,11 +51,6 @@ void setup() {
   Homie.setSetupFunction(setupHandler);
   Homie.setLoopFunction(loopHandler);
   
-  teplotaNode.advertise("unit");
-  teplotaNode.advertise("degrees");
-  vlhkostNode.advertise("unit");
-  vlhkostNode.advertise("percent");
-
   Homie.setup();
 }
 
